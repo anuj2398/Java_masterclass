4c9a2f273fd06406b531283a94f13460a59fc0f3
Anuj Kumar Deka <59406385+anuj2398@users.noreply.github.com>
2022-10-27 00:41:51 +0530

    Create Input Calculator.java

---
+++ Input Calculator.java
@@ -0,0 +1,55 @@
+Write a method called inputThenPrintSumAndAverage that does not have any parameters.
+
+The method should not return anything (void) and it needs to keep reading int numbers from the keyboard.
+
+When the user enters something that is not an int then it needs to print a message in the format "SUM = XX AVG = YY".
+
+XX represents the sum of all entered numbers of type int.
+YY represents the calculated average of all numbers of type long.
+
+
+
+EXAMPLES OF INPUT/OUTPUT:
+
+EXAMPLE 1:
+
+INPUT:
+
+1
+2
+3
+4
+5
+a
+OUTPUT
+
+SUM = 15 AVG = 3
+
+
+
+EXAMPLE 2:
+
+INPUT:
+
+hello
+
+OUTPUT:
+
+SUM = 0 AVG = 0
+  ------------------------------------------------------------------------------------------------------------------------
+  
+  import java.util.Scanner;
+ 
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        int sum=0;
+        double count=0;
+        while(scanner.hasNextInt()){
+            sum+=scanner.nextInt();
+            count++;
+        }
+    System.out.println("SUM = "+sum+" AVG = "+Math.round((double)sum/count));
+    scanner.close();
+}
+}
