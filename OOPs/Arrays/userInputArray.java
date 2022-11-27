d53ab20e0aad8da7f34acd0405c5560d4bb6fb37
Anuj Kumar Deka <59406385+anuj2398@users.noreply.github.com>
2022-11-27 14:03:47 +0530

    Update userInputArray.java

--- OOPs/Arrays/userInputArray.java
+++ OOPs/Arrays/userInputArray.java
@@ -9,6 +9,7 @@ public class Main {
         for(int j=0;j<myIntergers.length;j++){
             System.out.println("the element at "+j+" is "+myIntergers[j]);
         }
+        System.out.println("the average is "+getaverage(myIntergers));
     }
     public static int[] getIntegers(int num){
         int[] arr=new int[num];
@@ -17,5 +18,12 @@ public class Main {
             arr[i]=scanner.nextInt();
         }
         return arr;
+    }
+    public static double getAverage(int[] array){
+        int sum=0;
+        for(int i=0;i<array.length;i++){
+            sum+=array[i];
+        }
+        return (double) sum /  array.length;
     }
 }
